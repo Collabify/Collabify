e016f4944374fed4dc2de778c5f6218008a9cc62
Erik <ejolson9@hotmail.com>
2015-03-14 12:25:25 -0500

    hopefully this will fix shit

--- app/src/main/java/space/collabify/collabify/activities/ModeSelectActivity.java
+++ app/src/main/java/space/collabify/collabify/activities/ModeSelectActivity.java
@@ -1,8 +1,14 @@
 package space.collabify.collabify.activities;
 
+import android.os.Bundle;
+
 import space.collabify.collabify.*;
 /**
  * This file was born on March 11 at 13:59
  */
 public class ModeSelectActivity extends CollabifyActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
--- app/src/main/java/space/collabify/collabify/activities/SettingsActivity.java
+++ app/src/main/java/space/collabify/collabify/activities/SettingsActivity.java
@@ -1,6 +1,5 @@
 package space.collabify.collabify.activities;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
