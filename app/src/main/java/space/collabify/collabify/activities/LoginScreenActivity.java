6fbe4c66eae0f55271da36924638b9de7469939d
Erik <ejolson9@hotmail.com>
2015-03-13 23:35:52 -0500

    spotify login activity shows up

--- app/src/main/java/space/collabify/collabify/activities/LoginScreenActivity.java
+++ app/src/main/java/space/collabify/collabify/activities/LoginScreenActivity.java
@@ -1,11 +1,85 @@
 package space.collabify.collabify.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.playback.ConnectionStateCallback;
+
 import space.collabify.collabify.*;
 
 /**
  * This file was born on March 11 at 13:57
  */
-public class LoginScreenActivity extends CollabifyActivity {
+public class LoginScreenActivity extends CollabifyActivity implements ConnectionStateCallback {
+    private final String TAG = getClass().getSimpleName();
+
+    private static final String CLIENT_ID = "a61533696d61400dac8c6dceb884a575";
+    private static final String REDIRECT_URI = "collabify-space-login://callback";
+
+    //request code that will be used to verify if the result comes from correct activity
+    //can be any integer
+    private static final int REQUEST_CODE = 1337;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+
+        // TODO: at this point don't know if the user will need streaming scope, will have to be done on choose mode
+        builder.setScopes(new String[]{"user-read-private", "user-library-read", "user-read-private"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            //TODO: do something with response?
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d(TAG, "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d(TAG, "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable throwable) {
+        Log.d(TAG, "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d(TAG, "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+        Log.d(TAG, "Received connection message: " + s);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 
 }
