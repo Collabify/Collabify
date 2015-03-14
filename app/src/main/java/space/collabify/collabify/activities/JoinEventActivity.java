117bdb5ad2b6d92e79ae3a446c88c4a87879eaeb
Erik Olson <ejolson9@hotmail.com>
2015-03-14 01:02:06 -0500

    Revert "Login activity"

--- app/src/main/java/space/collabify/collabify/activities/JoinEventActivity.java
+++ app/src/main/java/space/collabify/collabify/activities/JoinEventActivity.java
@@ -1,9 +1,9 @@
 package space.collabify.collabify.activities;
 
+import space.collabify.collabify.*;
 
 /**
  * This file was born on March 11 at 14:00
  */
-public class JoinEventActivity extends CollabifyActivity{
-
+public class JoinEventActivity extends CollabifyActivity {
 }
--- app/src/main/java/space/collabify/collabify/activities/LoginScreenActivity.java
+++ app/src/main/java/space/collabify/collabify/activities/LoginScreenActivity.java
@@ -1,98 +1,11 @@
 package space.collabify.collabify.activities;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-
-import com.spotify.sdk.android.authentication.AuthenticationClient;
-import com.spotify.sdk.android.authentication.AuthenticationRequest;
-import com.spotify.sdk.android.authentication.AuthenticationResponse;
-import com.spotify.sdk.android.playback.ConnectionStateCallback;
-
 import space.collabify.collabify.*;
 
 /**
  * This file was born on March 11 at 13:57
  */
-public class LoginScreenActivity extends CollabifyActivity implements ConnectionStateCallback {
-    private final String TAG = getClass().getSimpleName();
-
-    private static final String CLIENT_ID = "a61533696d61400dac8c6dceb884a575";
-    private static final String REDIRECT_URI = "collabify-space-login://callback";
-
-    //request code that will be used to verify if the result comes from correct activity
-    //can be any integer
-    private static final int REQUEST_CODE = 1337;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        // TODO: at this point don't know if the user will need streaming scope, will have to be done on choose mode
-        //may want to have some sort of 'load screen' that just shows our icon and name for like two seconds
-        // before prompting the user to sign up through spotify
-        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
-                AuthenticationResponse.Type.TOKEN,
-                REDIRECT_URI);
-
-        builder.setScopes(new String[]{"user-read-private", "user-library-read", "user-read-private"});
-        AuthenticationRequest request = builder.build();
-        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-
-        // Check if result comes from the correct activity
-        if (requestCode == REQUEST_CODE) {
-            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
-
-            //checking for successful login response
-            String error = response.getError();
-            if(error == null){
-                //good to move on?...
-                //TODO: something with the response.accessToken() but not sure what yet
-                Intent i = new Intent(this, ModeSelectActivity.class);
-                startActivity(i);
-            }else{
-                //TODO: maybe have button to try spotify login again,
-                // and show text or some shit?
-            }
-        }
-    }
-
-    @Override
-    public void onLoggedIn() {
-        Log.d(TAG, "User logged in");
-    }
-
-    @Override
-    public void onLoggedOut() {
-        Log.d(TAG, "User logged out");
-    }
-
-    @Override
-    public void onLoginFailed(Throwable throwable) {
-        Log.d(TAG, "Login failed");
-    }
-
-    @Override
-    public void onTemporaryError() {
-        Log.d(TAG, "Temporary error occurred");
-    }
-
-    @Override
-    public void onConnectionMessage(String s) {
-        Log.d(TAG, "Received connection message: " + s);
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+public class LoginScreenActivity extends CollabifyActivity {
 
 
 }
