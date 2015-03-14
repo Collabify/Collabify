b9b98623b323767d8dab2cbb4505e1dc4618cd49
Erik <ejolson9@hotmail.com>
2015-03-14 00:10:57 -0500

    added hamburger button to action bar for collabifyActivity, and a settings action that launches a dummy settings activity.  specific settings for either dj/collabifier will likely just be a fragment inside this activity

--- app/src/main/java/space/collabify/collabify/activities/CollabifyActivity.java
+++ app/src/main/java/space/collabify/collabify/activities/CollabifyActivity.java
@@ -1,7 +1,12 @@
 package space.collabify.collabify.activities;
 
 import space.collabify.collabify.*;
+
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * This file was born on March 11 at 13:44
@@ -11,11 +16,36 @@ public class CollabifyActivity extends ActionBarActivity {
     protected ServerManager mServerManager;
 
     public CollabifyActivity(){
-
     }
 
     public CollabifyActivity(AppManager mAppManager, ServerManager mServerManager) {
         this.mAppManager = mAppManager;
         this.mServerManager = mServerManager;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_collabify_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle presses on the action bar items
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /** Launch settings activity */
+    private void openSettings() {
+        Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(intent);
     }
 }
