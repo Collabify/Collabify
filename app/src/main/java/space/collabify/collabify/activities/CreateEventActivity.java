3436ab4eb5374d3ee92625e170839b9b69da0b5d
Andrew McOlash <amcolash@gmail.com>
2015-03-17 16:34:01 -0500

    Mode selections and change activities, with buttons

--- app/src/main/java/space/collabify/collabify/activities/CreateEventActivity.java
+++ app/src/main/java/space/collabify/collabify/activities/CreateEventActivity.java
@@ -1,6 +1,8 @@
 package space.collabify.collabify.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import space.collabify.collabify.*;
 
@@ -12,5 +14,14 @@ public class CreateEventActivity extends CollabifyActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+    }
+
+    public void toModeSelect(View view) {
+      finish();
+    }
+
+    public void toDj(View view) {
+      Intent intent = new Intent(this, DjActivity.class);
+      startActivity(intent);
     }
 }
