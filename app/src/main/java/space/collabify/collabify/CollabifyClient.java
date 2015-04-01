034768c7c332309b2b4635971f72408d0698216a
Erik <ejolson9@hotmail.com>
2015-04-01 18:07:59 -0500

    reformatted to make merge easier

--- app/src/main/java/space/collabify/collabify/CollabifyClient.java
+++ app/src/main/java/space/collabify/collabify/CollabifyClient.java
@@ -17,108 +17,110 @@ import space.collabify.collabify.Json;
  */
 public class CollabifyClient {
 
-  private static CollabifyClient instance;
+    private static CollabifyClient instance;
 
-  private boolean eventUpdating;
-  private boolean usersUpdating;
+    private boolean eventUpdating;
+    private boolean usersUpdating;
 
-  // TODO: Find a better place for endpoints
-  private String EVENTS = "http://collabify.space/event.json";
+    // TODO: Find a better place for endpoints
+    private String EVENTS = "http://collabify.space/event.json";
 
-  private CollabifyClient() {
+    private CollabifyClient() {
 
-  }
-
-  public static CollabifyClient getInstance() {
-    if (instance == null) {
-      instance = new CollabifyClient();
     }
-    return instance;
-  }
 
-  /**
-   * Queries the server for events
-   * @param request query information
-   * @return list of events based off of request param
-   */
-  public List<Event> getEvents(LoadEventsRequest request){
-    //TODO fill in with actual server stuff
-    eventUpdating = true;
-
-    ArrayList<Event> events = new ArrayList<>();
-
-    // Get json data
-    JSONArray jArray = Json.getJsonArray("events", EVENTS);
-    if (jArray != null) {
-      for (int i = 0; i < jArray.length(); i++) {
-        try {
-          JSONObject oneObject = jArray.getJSONObject(i);
-          // Pulling items from the array
-          String name = oneObject.getString("name");
-          Boolean passwordProtected = oneObject.getBoolean("passwordProtected");
-          String password = oneObject.getString("password");
-          events.add(new Event(name, passwordProtected, password));
-        } catch (Exception e) {
-          events.add(new Event("Whoops, something went wrong!", false, null));
-          events.add(new Event("Please pull to refresh", false, null));
+    public static CollabifyClient getInstance() {
+        if (instance == null) {
+            instance = new CollabifyClient();
         }
-      }
-      eventUpdating = false;
-    } else {
-      events.add(new Event("Whoops, something went wrong!", false, null));
-      events.add(new Event("Please pull to refresh", false, null));
+        return instance;
     }
 
-    // TODO: On successful return of event list
-    return events;
-  }
+    /**
+     * Queries the server for events
+     *
+     * @param request query information
+     * @return list of events based off of request param
+     */
+    public List<Event> getEvents(LoadEventsRequest request) {
+        //TODO fill in with actual server stuff
+        eventUpdating = true;
 
-  public boolean isEventUpdating() {
-    return eventUpdating;
-  }
+        ArrayList<Event> events = new ArrayList<>();
 
-  public void joinEvent(Event event, User user){
-    //TODO: implementation
-  }
+        // Get json data
+        JSONArray jArray = Json.getJsonArray("events", EVENTS);
+        if (jArray != null) {
+            for (int i = 0; i < jArray.length(); i++) {
+                try {
+                    JSONObject oneObject = jArray.getJSONObject(i);
+                    // Pulling items from the array
+                    String name = oneObject.getString("name");
+                    Boolean passwordProtected = oneObject.getBoolean("passwordProtected");
+                    String password = oneObject.getString("password");
+                    events.add(new Event(name, passwordProtected, password));
+                } catch (Exception e) {
+                    events.add(new Event("Whoops, something went wrong!", false, null));
+                    events.add(new Event("Please pull to refresh", false, null));
+                }
+            }
+            eventUpdating = false;
+        } else {
+            events.add(new Event("Whoops, something went wrong!", false, null));
+            events.add(new Event("Please pull to refresh", false, null));
+        }
 
-  /**
-   * Queries the server for users
-   * @param request query information
-   * @return list of events based off of request param
-   */
-  public List<User> getUsers(LoadUsersRequest request){
-    //TODO fill in with actual server stuff
-
-    usersUpdating = true;
-
-    //fake some server delay
-    try {
-      Thread.sleep(2000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
+        // TODO: On successful return of event list
+        return events;
     }
 
-    ArrayList<User> users = new ArrayList<>();
-    users.add(new User("Best Singers Ever!!!", 0));
-    users.add(new User("Katy Perry", 1));
-    users.add(new User("Taylor Swift", 2));
-    users.add(new User("Miley Cyrus", 3));
-    users.add(new User("Spice Girls", 4));
-    users.add(new User("Beyonce", 5));
-    users.add(new User("Madonna", 6));
-    users.add(new User("Brittney Spears", 7));
-    users.add(new User("Lady Gaga", 8));
-    users.add(new User("Avril Lavigne", 9));
-    users.add(new User("P!nk", 10));
-    users.add(new User("Alanis Morissette", 11));
+    public boolean isEventUpdating() {
+        return eventUpdating;
+    }
 
-    usersUpdating = false;
+    public void joinEvent(Event event, User user) {
+        //TODO: implementation
+    }
 
-    return users;
-  }
+    /**
+     * Queries the server for users
+     *
+     * @param request query information
+     * @return list of events based off of request param
+     */
+    public List<User> getUsers(LoadUsersRequest request) {
+        //TODO fill in with actual server stuff
 
-  public boolean isUsersUpdating() {
-    return usersUpdating;
-  }
+        usersUpdating = true;
+
+        //fake some server delay
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("Best Singers Ever!!!", 0));
+        users.add(new User("Katy Perry", 1));
+        users.add(new User("Taylor Swift", 2));
+        users.add(new User("Miley Cyrus", 3));
+        users.add(new User("Spice Girls", 4));
+        users.add(new User("Beyonce", 5));
+        users.add(new User("Madonna", 6));
+        users.add(new User("Brittney Spears", 7));
+        users.add(new User("Lady Gaga", 8));
+        users.add(new User("Avril Lavigne", 9));
+        users.add(new User("P!nk", 10));
+        users.add(new User("Alanis Morissette", 11));
+
+        usersUpdating = false;
+
+        return users;
+    }
+
+    public boolean isUsersUpdating() {
+        return usersUpdating;
+    }
 
 }
--- app/src/main/java/space/collabify/collabify/activities/DjActivity.java
+++ app/src/main/java/space/collabify/collabify/activities/DjActivity.java
@@ -1,4 +1,5 @@
 package space.collabify.collabify.activities;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -15,106 +16,106 @@ import space.collabify.collabify.base.CollabifyActivity;
  * This file was born on March 11 at 14:02
  */
 public class DjActivity extends CollabifyActivity implements ActionBar.TabListener {
-  private ViewPager viewPager;
-  private TabsPagerAdapter mAdapter;
-  private ActionBar actionBar;
-  // Tab titles
-  private String[] tabs = {"Player", "Playlist", "DJ Tracks", "User List"};
-  private int[] icons = {R.drawable.ic_player, R.drawable.ic_playlist, R.drawable.ic_dj, R.drawable.ic_users};
+    private ViewPager viewPager;
+    private TabsPagerAdapter mAdapter;
+    private ActionBar actionBar;
+    // Tab titles
+    private String[] tabs = {"Player", "Playlist", "DJ Tracks", "User List"};
+    private int[] icons = {R.drawable.ic_player, R.drawable.ic_playlist, R.drawable.ic_dj, R.drawable.ic_users};
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_dj);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dj);
 
-    // Initilization
-    viewPager = (ViewPager) findViewById(R.id.djPager);
-    viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-      @Override
-      public void onPageSelected(int position) {
-        // on changing the page
-        // make respected tab selected
-        actionBar.setSelectedNavigationItem(position);
-        }
+        // Initilization
+        viewPager = (ViewPager) findViewById(R.id.djPager);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // on changing the page
+                // make respected tab selected
+                actionBar.setSelectedNavigationItem(position);
+            }
 
-      @Override
-      public void onPageScrolled(int arg0, float arg1, int arg2) {
-      }
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
 
-      @Override
-      public void onPageScrollStateChanged(int arg0) {
-      }
-    });
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
 
 
-    actionBar = getSupportActionBar();
-    mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+        actionBar = getSupportActionBar();
+        mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
 
-    viewPager.setAdapter(mAdapter);
-    actionBar.setHomeButtonEnabled(false);
-    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        viewPager.setAdapter(mAdapter);
+        actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-    // Adding Tabs
-    for (int i=0; i < tabs.length; i++) {
+        // Adding Tabs
+        for (int i = 0; i < tabs.length; i++) {
 //      actionBar.addTab(actionBar.newTab()
 //        .setText(tabs[i])
 //        .setIcon(icons[i])
 //        .setCustomView(R.layout.tab_layout)
 //        .setTabListener(this));
-      View tabView = getLayoutInflater().inflate(R.layout.tab_layout, null);
-      TextView tabText = (TextView) tabView.findViewById(R.id.tabText);
-      tabText.setText(tabs[i]);
+            View tabView = getLayoutInflater().inflate(R.layout.tab_layout, null);
+            TextView tabText = (TextView) tabView.findViewById(R.id.tabText);
+            tabText.setText(tabs[i]);
 
-      ImageView tabImage = (ImageView) tabView.findViewById(R.id.tabIcon);
-      tabImage.setImageDrawable(getResources().getDrawable(icons[i]));
+            ImageView tabImage = (ImageView) tabView.findViewById(R.id.tabIcon);
+            tabImage.setImageDrawable(getResources().getDrawable(icons[i]));
 
-      actionBar.addTab(actionBar.newTab()
-        .setCustomView(tabView)
-        .setTabListener(this));
+            actionBar.addTab(actionBar.newTab()
+                    .setCustomView(tabView)
+                    .setTabListener(this));
+        }
+
     }
 
-  }
+    @Override
+    public void onBackPressed() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // construct the dialog
+        builder.setTitle(R.string.title_end_event);
+        builder.setMessage(R.string.label_end_event);
 
-  @Override
-  public void onBackPressed() {
-    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    // construct the dialog
-    builder.setTitle(R.string.title_end_event);
-    builder.setMessage(R.string.label_end_event);
+        // exit if OK button pressed
+        builder.setPositiveButton(getString(R.string.ok_button),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }
+        );
 
-    // exit if OK button pressed
-    builder.setPositiveButton(getString(R.string.ok_button),
-      new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-          finish();
-        }
-      }
-    );
+        // close dialog on Cancel button pressed
+        builder.setNegativeButton(getString(R.string.cancel_button),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                }
+        );
 
-    // close dialog on Cancel button pressed
-    builder.setNegativeButton(getString(R.string.cancel_button),
-      new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-          dialog.cancel();
-        }
-      }
-    );
+        builder.show();
+    }
 
-    builder.show();
-  }
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
 
-  @Override
-  public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-    viewPager.setCurrentItem(tab.getPosition());
-  }
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
+    }
 
-  @Override
-  public void onTabUnselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-  }
-
-  @Override
-  public void onTabReselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-  }
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
+    }
 }
