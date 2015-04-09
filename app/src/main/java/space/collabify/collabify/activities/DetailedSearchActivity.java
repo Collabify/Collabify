8c073a81c03ef3975419f35d0a1084700fbcb2e0
Brandon Hill <thefatmatcat@gmail.com>
2015-04-09 13:10:50 -0500

    add song dialog pops up; next is to make server call to add song

--- app/src/main/java/space/collabify/collabify/activities/DetailedSearchActivity.java
+++ app/src/main/java/space/collabify/collabify/activities/DetailedSearchActivity.java
@@ -1,7 +1,9 @@
 package space.collabify.collabify.activities;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -66,6 +68,28 @@ public class DetailedSearchActivity extends ListActivity {
         }
     }
 
+    private void setupAddDialog(final String songDescription) {
+        // prompt to add song
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.add_song_dialog_title));
+        builder.setMessage(songDescription);
+        builder.setPositiveButton(getString(R.string.add_song_dialog_positive_text),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO: send song to server
+                    }
+                });
+        builder.setNegativeButton(getString(R.string.add_song_dialog_negative_text),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        builder.show();
+    }
+
     /**
      * Handles the display of events in a row
      */
@@ -84,9 +108,9 @@ public class DetailedSearchActivity extends ListActivity {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             View customView = inflater.inflate(R.layout.song_details_row, parent, false);
 
-            Song song = getItem(position);
+            final Song song = getItem(position);
             TextView songId = (TextView) customView.findViewById(R.id.details_row_song_id);
-            TextView rowName = (TextView) customView.findViewById(R.id.song_row_title);
+            TextView rowDesc = (TextView) customView.findViewById(R.id.song_row_description);
             ImageView albumArt = (ImageView) customView.findViewById(R.id.song_details_album_art);
             ImageButton addButton = (ImageButton) customView.findViewById(R.id.song_row_add);
 
@@ -96,14 +120,14 @@ public class DetailedSearchActivity extends ListActivity {
             String artist = song.getArtist();
             artist = artist.substring(0, Math.min(artist.length(), 30));
 
-            String newSongDescription = title + "\n(" + artist + ")";
+            final String newSongDescription = title + "\n(" + artist + ")";
 
-            rowName.setText(newSongDescription);
+            rowDesc.setText(newSongDescription);
 
             addButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    // TODO: have popup for adding song on click
+                    setupAddDialog(newSongDescription);
                 }
             });
 
